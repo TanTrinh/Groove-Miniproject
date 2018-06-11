8a68c996b79141111165caa8dd599263bb73cb19
HuyVu <nguyenhuyvu08@gmail.com>
2018-06-11 11:53:01 +0700

    initial

---
+++ GoGo/Infrastructures/Infrastructures.SeedData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Infrastructures.SeedData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ GoGo/Infrastructures/Infrastructures/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Infrastructures
+{
+    public class Class1
+    {
+    }
+}
