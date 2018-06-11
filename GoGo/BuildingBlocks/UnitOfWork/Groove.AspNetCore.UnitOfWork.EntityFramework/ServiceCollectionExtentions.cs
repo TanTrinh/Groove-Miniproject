da0a89fb7be3ab6880ba52f6ed2fdc10bd1c5999
HuyVu <nguyenhuyvu08@gmail.com>
2018-06-11 13:40:31 +0700

    Add building Blocks

---
+++ GoGo/BuildingBlocks/UnitOfWork/Groove.AspNetCore.UnitOfWork.EntityFramework/ServiceCollectionExtentions.cs
@@ -0,0 +1,20 @@
+﻿using Groove.AspNetCore.UnitOfWork;
+using Groove.AspNetCore.UnitOfWork.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtentions
+    {
+        public static void AddUnitOfWork<TDbContext>(this IServiceCollection services) where TDbContext : DbContext, IUnitOfWorkContext
+        {
+            services.AddScoped<IUnitOfWorkContext, TDbContext>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+
+    }
+}
---
+++ GoGo/BuildingBlocks/UnitOfWork/Groove.AspNetCore.UnitOfWork/IUnitOfWorkContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Groove.AspNetCore.UnitOfWork
+{
+    public interface IUnitOfWorkContext
+    {
+        object GetUnitOfWorkContext();
+    }
+}
