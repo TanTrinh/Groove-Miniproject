7d83e24a7f969454c2cc5ebbadd1454cf02a71ea
nntdinh3896 <dinh.nguyen@groovetechnology.com>
2018-07-09 18:09:25 +0700

    LIM2018-136

--- GoGo/Applications/GoGoSpa/Client-App/src/app/shared/models/utilities.ts
+++ GoGo/Applications/GoGoSpa/Client-App/src/app/shared/models/utilities.ts
@@ -1,3 +1,10 @@
 export class StringObject {
   content: string;
 }
+
+export class RequestStatus {
+  public readonly Inactive = 'Inactive';
+  public readonly Waiting = 'Waiting';
+  public readonly Accepted = 'Accepted';
+  public readonly Rejected = 'Rejected';
+}
